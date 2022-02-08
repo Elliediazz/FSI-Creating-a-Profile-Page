3678dd6c2ac58f365837b4ffbc218987f0522996
Ellie Diaz <elliediaz@Ellies-Air.lan>
2022-02-08 13:44:32 -0600

    Added attributes and image

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 let content = document.querySelector('.js-generated.content')
+
+let header = document.createElement('h1')
+header.setAttribute('class', 'dog-name')
+header.textContent = 'Rizzo'
+content.append(header)
+
+let dogContent = document.createElement('div')
+dogContent.setAttribute('class', 'dog-content')
+content.append(dogContent)
+
+let img= document.createElement('img')
+img.setAttribute('class', 'dog-image')
+img.setAttribute('src', './assets/rizzo.jpg')
+dogContent.append(img)
